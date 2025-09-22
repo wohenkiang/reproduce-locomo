ad8453b43423f6e6299404de957ec7bd51e756e2
young <2523386328@qq.com>
2025-09-22 21:43:31 +0800

    a

--- scripts/evaluate_hf_llm.sh
+++ scripts/evaluate_hf_llm.sh
@@ -3,7 +3,7 @@ source scripts/env.sh
 
 
 # run models
-for MODEL in llama3-chat-70b llama3-chat-70b mistral-instruct-7b-32k-v2; do
+for MODEL in mistral-instruct-7b-32k-v2; do
     python3 task_eval/evaluate_qa.py \
         --data-file $DATA_FILE_PATH --out-file $OUT_DIR/$QA_OUTPUT_FILE \
         --model $MODEL --use-4bit --batch-size 1
