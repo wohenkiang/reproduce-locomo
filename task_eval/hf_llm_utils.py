4b6c1a506e63fbc08c19db8291d30318fff4267c
young <2523386328@qq.com>
2025-09-22 22:01:09 +0800

    aaa

--- task_eval/hf_llm_utils.py
+++ task_eval/hf_llm_utils.py
@@ -340,7 +340,7 @@ def init_hf_model(args):
     else:
         raise ValueError
     
-    hf_token = os.environ['HF_TOKEN']
+    hf_token = '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'
     huggingface_hub.login(hf_token)
 
     if args.use_4bit:
