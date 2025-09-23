6031cbeec0b15daafd34245b7441777721fe3049
young <2523386328@qq.com>
2025-09-24 00:46:30 +0800

    update

--- task_eval/hf_llm_utils.py
+++ task_eval/hf_llm_utils.py
@@ -339,7 +339,7 @@ def init_hf_model(args):
     else:
         raise ValueError
 
-    hf_token = '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'
+    hf_token = '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'
     print("here!!!!!")
     print(hf_token)
     huggingface_hub.login(hf_token)
